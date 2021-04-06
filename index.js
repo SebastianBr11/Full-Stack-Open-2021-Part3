4f03d387459c3c4e71d1c0ce294e702293b89fde
SebiPlayzZ <41546861+SebiPlayzZ@users.noreply.github.com>
2021-04-06 17:32:16 +0200

    3.8: Phonebook backend step8

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const app = express()
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
 
-app.use(morgan('tiny'))
+const app = express()
 app.use(express.json())
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body'),
+)
 
 let persons = [
   {
